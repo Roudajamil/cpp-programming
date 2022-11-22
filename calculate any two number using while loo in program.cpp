dec36a5099f33b11a15b4e079c92356906b6e945
Roudajamil <118732618+Roudajamil@users.noreply.github.com>
2022-11-22 07:03:44 -0800

    Create calculate any two number using while loo in program.cpp

---
+++ calculate any two number using while loo in program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueOne;
+	int valueTwo;
+	cout<<"Enter value number one";
+	cin>>valueOne;
+	cout<<"Enter value number two";
+		cin>>valueTwo;
+		cout<<"The result is"<<valueOne+valueTwo;
+}
