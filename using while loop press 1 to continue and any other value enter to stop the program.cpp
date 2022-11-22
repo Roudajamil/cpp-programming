04023ed73d945ea7f68e00cd34d92605860ea46a
Roudajamil <118732618+Roudajamil@users.noreply.github.com>
2022-11-22 06:56:08 -0800

    Create using while loop press 1 to continue and any other value enter to stop the program.cpp

---
+++ using while loop press 1 to continue and any other value enter to stop the program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i = 1;
+	while(i>=1)
+	{
+		cout<<"while loop is working fine\n";
+		cout<<"press 1 to continue any other key to stop\n";
+		cin>>i;
+	}
+}
