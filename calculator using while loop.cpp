6f44ac443591689c8b750d0fe6d581521757d251
Roudajamil <118732618+Roudajamil@users.noreply.github.com>
2022-11-24 01:38:28 -0800

    Create calculator using while loop.cpp

---
+++ calculator using while loop.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int valueOne;
+	int valueTwo;
+	{
+	  int i=1;
+	  while(i==1)
+	  {
+	   cout<<"Please Enter value number one";
+	   cin>>valueOne;
+	   cout<<"Please Enter value number two";
+	   cin>>valueTwo;
+	   cout<<"The Result is"<<valueOne+valueTwo<<endl;
+	   cout<<"Press 1 to continue any other key to stop:";
+	   cin>>i;
+	
+	   cout<<" Thank you for using the program";
+      }
+    }
+}
